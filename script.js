2e453564bcb2a691320f6e3eaf1ea57a4ba37aa6
Lente Bakker <lentebakker@gmail.com>
2019-10-04 14:03:19 +0200

    script.js

--- script.js
+++ script.js
@@ -1,6 +1,37 @@
-var woorden;
+var randomWoorden;
+var gekozenLetter;
+var aantalLevens;
+var maxAantalLevens;
+var alfabet;
+var buttonGuess;
+var buttonStart;
 
-woorden=['dehydreren', 'exogeen','panorama', 'belegging', 'metalen'];
+randomWoorden = ['dehydreren', 'exogeen', 'panorama', 'belegging', 'metalen'];
+alfabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-var wordPicker = Math.floor(Math.random()*woorden.length);
-var juisteWoord = landen[wordPicker];
+var wordPicker = Math.floor(Math.random() * randomWoorden.length);
+var juisteWoord = randomWoorden[wordPicker];
+
+
+/*- Pak het random gekozen woord uit de array
+kijk hoeveel letters het zijn en print zoveel
+streepse als de lente van het woord-->
+<h1><script>document.write("- - - - - - - - ");</script></h1>*/
+
+
+    for (var i = 0; i < juisteWoord.length; i++) {
+       
+
+    }       
+
+    document.getElementById('').onkeypress= returnGekozenLetter;
+
+function returnGekozenLetter(key)
+
+    /*Als gebruiker heb ik 5 levens, dit wordt met tekst weergegeven. Als de letter fout is gaat er 1 leven vanaf*/
+document.getElementById("") = maxAantalLevens;
+
+
+
+
+
