ac97e02fef7c52e04b91b7d9cdd06974cb4d3b93
Fleur <fleur.schneiders@live.nl>
2019-10-04 12:46:43 +0200

    HTML Pagina globaal afgemaakt

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ var woorden;
 
 woorden=['dehydreren', 'exogeen','panorama', 'belegging', 'metalen'];
 
+
+
 var wordPicker = Math.floor(Math.random()*woorden.length);
 var juisteWoord = woorden[wordPicker];
