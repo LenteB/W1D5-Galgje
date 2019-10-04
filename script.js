9a2e42e1012c1cd76a03d8345e5b8683764ddc5b
FCE-Schneiders <55957222+FCE-Schneiders@users.noreply.github.com>
2019-10-04 17:58:41 +0200

    in html veld dat ingevuld wordt doorgegeven wordt aan JS-file en die print invoer uit in html'
    in html veld dat ingevuld wordt doorgegeven wordt aan JS-file en die print invoer uit in html'

--- script.js
+++ script.js
@@ -7,22 +7,23 @@ let buttonStart;
 let randomWoorden = ['dehydreren', 'exogeen', 'panorama', 'belegging', 'metalen'];
 let alfabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
-let pickedWord= randomWoorden[Math.random() * randomWoorden.length | 0];
-console.log("Current Word: " + "_ ".repeat(pickedWord.length));
+const wordPicker= randomWoorden[Math.random() * randomWoorden.length | 0];
+console.log("Current Word: " + "_ ".repeat(wordPicker.length));
 
-/*Split pickedWord in aparte letters */
-let str = (pickedWord);
-let res = str.split("");
-/*test*/
-console.log(res);
+function checkGuessInput(){
+    let inputText= document.getElementById("guessTextInput").value;
+    document.write(inputText);
+}
 
-/* Loop: Komt de gekozenLetter overeen met 1 of meer van de juisteLetters array? 
-    Ja: weergeef letter(s) op de juiste plaats
-    Nee: er verdwijnt 1 van de 5 levens */
-
-    
-
-
- /*Als gebruiker heb ik 5 levens, dit wordt met tekst weergegeven. Als de letter fout is gaat er 1 leven vanaf
+/*- Pak het random gekozen woord uit de array
+kijk hoeveel letters het zijn en print zoveel
+streepse als de lente van het woord-->
+<h1><script>document.write("- - - - - - - - ");</script></h1>
+    for (let i = 0; i < juisteWoord.length; i++) {
+       
+    }       
+    document.getElementById('').onkeypress= returnGekozenLetter;
+function returnGekozenLetter()
+    /*Als gebruiker heb ik 5 levens, dit wordt met tekst weergegeven. Als de letter fout is gaat er 1 leven vanaf
+document.getElementById("") = maxAantalLevens;
 */
-
