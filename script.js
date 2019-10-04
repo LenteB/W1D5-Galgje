c649332d7acf91813fbe17c939d73dc77c1aaca4
FCE-Schneiders <55957222+FCE-Schneiders@users.noreply.github.com>
2019-10-04 17:59:42 +0200

    in html veld dat ingevuld wordt doorgegeven wordt aan JS-file en die print invoer uit in html'
    in html veld dat ingevuld wordt doorgegeven wordt aan JS-file en die print invoer uit in html'

--- script.js
+++ script.js
@@ -1,8 +1,29 @@
-var woorden;
+let gekozenLetter;
+let aantalLevens;
+let maxAantalLevens;
+let buttonGuess;
+let buttonStart;
 
-woorden=['dehydreren', 'exogeen','panorama', 'belegging', 'metalen'];
+let randomWoorden = ['dehydreren', 'exogeen', 'panorama', 'belegging', 'metalen'];
+let alfabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 
+const wordPicker= randomWoorden[Math.random() * randomWoorden.length | 0];
+console.log("Current Word: " + "_ ".repeat(wordPicker.length));
 
+function checkGuessInput(){
+    let inputText= document.getElementById("guessTextInput").value;
+    document.write(inputText);
+}
 
-var wordPicker = Math.floor(Math.random()*woorden.length);
-var juisteWoord = woorden[wordPicker];
+/*- Pak het random gekozen woord uit de array
+kijk hoeveel letters het zijn en print zoveel
+streepse als de lente van het woord-->
+<h1><script>document.write("- - - - - - - - ");</script></h1>
+    for (let i = 0; i < juisteWoord.length; i++) {
+       
+    }       
+    document.getElementById('').onkeypress= returnGekozenLetter;
+function returnGekozenLetter()
+    /*Als gebruiker heb ik 5 levens, dit wordt met tekst weergegeven. Als de letter fout is gaat er 1 leven vanaf
+document.getElementById("") = maxAantalLevens;
+*/
